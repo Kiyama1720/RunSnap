d84dc35c36097907dd37b891de82b35901ed410d
Haruki Kiyama <aladdin.magi101@gmail.com>
2021-06-15 10:17:41 +0000

    [Add] 新規登録、ログイン、ログアウト、新規投稿、詳細、編集、削除機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,42 @@
 class PostsController < ApplicationController
   def new
-    @post = Post.
+    @post = Post.new
+    p ENV["GOOGLE_MAP_API_KEY"]
+    gon.google_map_api_key = ENV["GOOGLE_MAP_API_KEY"]
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to post_path(@post.id)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    gon.google_map_api_key = ENV["GOOGLE_MAP_API_KEY"]
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    gon.google_map_api_key = ENV["GOOGLE_MAP_API_KEY"]
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post.id)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to "/"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :user_id, :body, :image, :address, :latitude, :longitude)
   end
 end
