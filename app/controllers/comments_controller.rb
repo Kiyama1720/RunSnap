33d1156f6b35716db4b06f3740f758484fe7a0b2
Haruki Kiyama <aladdin.magi101@gmail.com>
2021-06-27 20:10:07 +0000

    [Fix] エラー表記修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    comment = current_user.comments.new(post_comment_params)
-    comment.post_id = @post.id
-    comment.comment_number = 1
-    comment.save
+    @comment = current_user.comments.new(post_comment_params)
+    @comment.post_id = @post.id
+    @comment.comment_number = 1
+    @comment.save
     redirect_to post_path(@post.id)
   end
 
